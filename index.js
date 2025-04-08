2eba98363a0cf32670b95dd5f1f75dbf78ac89ae
Josia Scheerle <scheerle.josia@gmail.com>
2025-04-08 15:39:09 +0200

    fetch agenda from events

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const { churchtoolsClient, activateLogging } = require('@churchtools/churchtools
 const axiosCookieJarSupport = require('axios-cookiejar-support');
 const tough = require('tough-cookie');
 require('dotenv').config();
+const canvas = require('./canvas');
+
+canvas.create({
+    date: '13.04.2025', 
+    event: 'Familiengottesdienst',
+    subject: 'Hier steht das Thema\nder Predigt!'
+});
 
 churchtoolsClient.setCookieJar(axiosCookieJarSupport.wrapper, new tough.CookieJar());
 churchtoolsClient.setBaseUrl(process.env.BASE_URL);
 
 activateLogging();
-
-const user = {
-    username: process.env.USER,
-    password: process.env.PASSWORD
-}
 
 async function login(client, user) {
     let result = await client.post('/login', user)
@@ -24,28 +26,43 @@ async function login(client, user) {
     }
 }
 
-login(churchtoolsClient, user)
+async function getEvents() {
+    return formatEvents(await churchtoolsClient.get('/events'));
+}
 
-churchtoolsClient.get('/events').then(events => {
-    console.log('Events: ', events);
+async function getEventAgenda(eventId) {
+    const agenda = await churchtoolsClient.get(`/events/${eventId}/agenda`);
+    console.log('Agenda:', agenda);
+    return agenda;
+}
 
-    const formattedEvents = formatEvents(events);
-});
 
-
-function formatEvents(events) {
+async function formatEvents(events) {
     let formattedEvents = {};
-
     for(let i = 0; i < events.length; i++) {
         if (!events[i].note.includes('streaming: ja')) continue;
 
         if (!formattedEvents[events[i].guid]) {
             formattedEvents[events[i].guid] = {
                 date: new Date(events[i].startDate).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' }),
-                name: events[i].name,
+                eventName: events[i].name,
+                sermonSubject: '',
                 startTime: new Date(events[i].startDate).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }),
+                preacher: '',
+                scripture: '',
+                scriptureLink: '',
+                specialties: []
             }
         };
     };
     return formattedEvents;
 }
+
+const user = {
+    username: process.env.USER,
+    password: process.env.PASSWORD
+}
+
+login(churchtoolsClient, user)
+getEvents()
+getEventAgenda(38);
