c88af468d7c88a256bbfd3e84ea6a4e05711834c
Josia Scheerle <scheerle.josia@gmail.com>
2025-09-19 11:51:21 +0200

    Push to repository after months of local dev

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ThumbGen_ChurchTools
+
+- should run every week on Thursdays or Fridays
+- should get all Events for the next 7 Days including the next day of runtime (running on: Tu -> events: Fr-Tu)
+- somehow possible to redo it on updates?
+- only work on closed agendas with "streaming: ja"
--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const tough = require('tough-cookie');
 require('dotenv').config();
 const canvas = require('./canvas');
 
-canvas.create({
-    date: '13.04.2025', 
-    event: 'Familiengottesdienst',
-    subject: 'Hier steht das Thema\nder Predigt!'
-});
-
 churchtoolsClient.setCookieJar(axiosCookieJarSupport.wrapper, new tough.CookieJar());
 churchtoolsClient.setBaseUrl(process.env.BASE_URL);
 
@@ -27,7 +21,11 @@ async function login(client, user) {
 }
 
 async function getEvents() {
-    return formatEvents(await churchtoolsClient.get('/events'));
+    // let fromDate = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split('T')[0];
+    // let toDate = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString().split('T')[0]; 
+    const events = await churchtoolsClient.get(`/events`);
+    // console.log('Events:', events);
+    return await formatEvents(events);
 }
 
 async function getEventAgenda(eventId) {
@@ -36,32 +34,48 @@ async function getEventAgenda(eventId) {
     return agenda;
 }
 
-async function getResponsiblePerson(agendaEntry) {
-    if(!agendaEntry.responsible.text.includes('[')) return agendaEntry.text;
-    let responsible = agendaEntry.responsible.persons[0].person.title
-    if(responsible) return responsible;
-
+function getResponsiblePerson(agendaItem) {
+    if(!agendaItem.responsible.text.includes('[')) return agendaItem.text;
+    if(agendaItem.responsible.personns[0].accepted) return agendaItem.responsible.persons[0].person.title
+    throw Error("No user or not accepted user in service: " + agendaItem.responsible.persons[0].service)
 }
 
-async function formatEvents(events) {
-    let formattedEvents = {};
-    for(let i = 0; i < events.length; i++) {
-        if (!events[i].note.includes('streaming: ja')) continue;
+function getSermonInfo(agendaItem) {
+    if(agendaItem.note) {
+        let infos = agendaItem.note.split('\n');
+        infos[0] = infos[0].replace('Predigttext:', '').trim()
+        infos[1] = infos[1].replace('Thema:', '').trim()
+        return infos;
+    }
+}
 
-        if (!formattedEvents[events[i].guid]) {
-            formattedEvents[events[i].guid] = {
-                date: new Date(events[i].startDate).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' }),
-                eventName: events[i].name,
-                sermonSubject: '',
-                startTime: new Date(events[i].startDate).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }),
-                preacher: '',
-                scripture: '',
-                scriptureLink: '',
-                specialties: []
-            }
-        };
-    };
+
+async function formatEvents(events) {
+    let formattedEvents = [];
+    for(let i = 0; i < events.length; i++) {
+        if (!events[i].note.includes('streaming: ja')) {
+            console.log("No Livestream. Skipping event:", events[i].name);
+            continue;
+        } 
+
+        formattedEvents.push({
+            date: new Date(events[i].startDate).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit' }),
+            eventName: selectEvent(events[i].name),
+            sermonSubject: '',
+            startTime: new Date(events[i].startDate).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }),
+            preacher: '',
+            scripture: '',
+            scriptureLink: '',
+            specialties: []
+        })
+    }
     return formattedEvents;
+}
+
+function selectEvent(eventName) {
+    if (eventName.includes("Familien")) return 'Familiengottesdienst';
+    else if (eventName.includes("Jugend")) return 'Jugendgottesdienst';
+    else return 'Gottesdienst';
 }
 
 const user = {
@@ -70,5 +84,14 @@ const user = {
 }
 
 login(churchtoolsClient, user)
-getEvents()
-getEventAgenda(38);
+
+getEvents().then(events => {
+    const eventList = events;
+    console.log("Events:", eventList);
+    canvas.create({
+        date: eventList[0].date,
+        event: eventList[0].eventName,
+        subject: eventList[0].sermonSubject
+    })
+});
+// getEventAgenda(191);
